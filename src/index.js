4c68676da557b9651b0f7611e34fea7defd68aa4
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2022-09-17 18:09:01 +0300

    test hashrouter

--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import App2 from './App2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log(process.env.PUBLIC_URL);
 root.render(
   <React.StrictMode>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
           <Switch>
               <Route exact path="/" component={App} />
               <Route path="/tourney" component={App2} />
           </Switch>
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>
 );
