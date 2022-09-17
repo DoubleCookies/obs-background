a94789e463a826817468219d97c06246abfe6163
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2022-09-17 18:05:13 +0300

    fix?

--- src/index.js
+++ src/index.js
@@ -2,20 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App2 from './App2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const a1 = App();
-const a2 = App2();
 console.log(process.env.PUBLIC_URL);
 root.render(
   <React.StrictMode>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <Routes>
-              <Route exact path="/" element={a1} />
-              <Route path="/tourney" element={a2} />
-          </Routes>
+          <Switch>
+              <Route exact path="/" component={App} />
+              <Route path="/tourney" component={App2} />
+          </Switch>
       </BrowserRouter>
   </React.StrictMode>
 );
