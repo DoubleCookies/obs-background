ac64994bd3490c234ce18110006f3c27fc1ec447
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2022-11-27 12:47:07 +0300

    BasicBg dynamic export + sort

--- src/Components/BasicBg/BasicBg.js
+++ src/Components/BasicBg/BasicBg.js
@@ -1,62 +1,55 @@
-import bg from "../../Backgrounds/bg5.jpg";
-import bg2 from "../../Backgrounds/bg2-2.jpg";
 import React from 'react';
 import './BasicBg.css';
-
-/*
-Priority:
-bg5.jpg
-bg2-2.jpg
-bg1.webp
-bg-inv2.jpg
-bg-inv.jpg
-
-bg1.jpg
-bg2.webp
-bg4.webp
-b3.webp
-bg6.webp
- */
 
 class BasicBg extends React.Component {
     constructor(props) {
         super(props);
+        const images = this.importAll(require.context('../../Backgrounds',
+            false, /\.(png|jpe?g|svg|webp)$/));
+        console.log('images:', images);
         this.state = {
-            url: `url(${bg})`,
-            counter: 0
+            url: `url(${images[0]})`,
+            counter: 0,
+            images: images,
         }
     }
 
+    importAll(r) {
+        let images = [];
+        r.keys().forEach((item, index) => {
+            images[index] = r(item);
+        });
+        images.sort((a,b) => {
+            const slashA = a.lastIndexOf('/');
+            const slashB = b.lastIndexOf('/');
+
+            const dotA = a.indexOf('.');
+            const dotB = b.indexOf('.');
+            const numberA = a.substring(slashA + 2, dotA);
+            const numberB = b.substring(slashB + 2, dotB);
+            return (Number)(numberA) - (Number)(numberB);
+        })
+        return images;
+    }
+
     changeBg() {
-        const {counter} = this.state;
-        let newUrl;
-        console.log(counter)
-        switch ((counter + 1) % 2) {
-            case 0: {
-                newUrl = `url(${bg})`;
-                break;
-            }
-            case 1: {
-                newUrl = `url(${bg2})`;
-                break;
-            }
-            default: {
-                newUrl = `url(${bg})`;
-                break;
-            }
-        }
+        const {counter, images} = this.state;
+        let index = (counter + 1) % images.length;
+        let newUrl = `url(${images[index]})`;
         this.setState({
             counter: counter + 1,
             url: newUrl
-        })
+        });
     }
 
     render() {
         return (
             <div onClick={this.changeBg.bind(this)}
                  className="app"
-                 style={{backgroundImage: `${this.state.url}`,
-                     backgroundSize: 500}}
+                 style={{
+                     backgroundImage: `${this.state.url}`,
+                     backgroundSize: 500
+                 }}
             />
         );
     }
--- src/Components/EwfishSlideBg/EwfishSlideBg.js
+++ src/Components/EwfishSlideBg/EwfishSlideBg.js
@@ -1,22 +1,7 @@
-import bg from "../../Backgrounds/bg5.jpg";
+import bg from "../../Backgrounds/b1.jpg";
 import ew2 from "./ew2.webp";
 import React from 'react';
 import './EwfishSlideBg.css';
-
-/*
-Priority:
-bg5.jpg
-bg2-2.jpg
-bg1.webp
-bg-inv2.jpg
-bg-inv.jpg
-
-bg1.jpg
-bg2.webp
-bg4.webp
-b3.webp
-bg6.webp
- */
 
 function EwfishSlideBg() {
     return (
