31eff3e6fcbb6cfe83f45aaa876313298961788c
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2024-02-08 11:03:42 +0100

    refactor

--- src/Components/importHelper.js
+++ src/Components/importHelper.js
@@ -1,4 +1,5 @@
 
+// Get all images from folder
 export function importAll(r) {
     let images = [];
     if (!r) {
@@ -7,14 +8,11 @@ export function importAll(r) {
     r.keys().forEach((item, index) => {
         images[index] = r(item);
     });
-    images.sort((a,b) => {
-        const slashA = a.lastIndexOf('/');
-        const slashB = b.lastIndexOf('/');
 
-        const dotA = a.indexOf('.');
-        const dotB = b.indexOf('.');
-        const numberA = a.substring(slashA + 2, dotA);
-        const numberB = b.substring(slashB + 2, dotB);
+    // File names are in format .../.../b*number*.webp, so we need to extract *number* from it
+    images.sort((a,b) => {
+        const numberA = a.substring(a.lastIndexOf('/') + 2, a.indexOf('.'));
+        const numberB = b.substring(b.lastIndexOf('/') + 2, b.indexOf('.'));
         return (Number)(numberA) - (Number)(numberB);
     })
     return images;
